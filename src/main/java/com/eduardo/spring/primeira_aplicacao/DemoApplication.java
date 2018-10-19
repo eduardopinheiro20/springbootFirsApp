25dc86a2b84631dc112499d789ede9712dd93223
Eduardo Pinheiro <eduardopinheiro20@outlook.com>
2018-10-19 15:35:05 -0300

    secont commit

--- src/main/java/com/eduardo/spring/primeira_aplicacao/DemoApplication.java
+++ src/main/java/com/eduardo/spring/primeira_aplicacao/DemoApplication.java
@@ -2,12 +2,12 @@ package com.eduardo.spring.primeira_aplicacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
- 
-
+ //começo do código
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
 }
